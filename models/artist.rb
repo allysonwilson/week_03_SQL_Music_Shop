bda0d4710612f125e7fecbdb71ddf427f3fd11aa
Allyson Gayle Wilson <allysonmacwilson@gmail.com>
2017-08-16 15:45:12 +0100

    add save function with sql runner to artist

--- models/artist.rb
+++ models/artist.rb
@@ -1,12 +1,24 @@
 require('pg')
-
+require_relative('../db/sql_runner')
 class Artist
 
+    attr_accessor(:name)
+
   def initialize()
+    @id =
     @name = name
-    @album = album
+    # @album = album
 
   end
 
+  def save
+    sql = ' INSERT INTO artists (name)
+    VALUES ( $1) RETURNING *;'
+    values = [@name]
+
+    @id = returned_data[0]['id'].to_i()
+    SqlRunner.run(sql, [@id])
+
+  end
 
 end
